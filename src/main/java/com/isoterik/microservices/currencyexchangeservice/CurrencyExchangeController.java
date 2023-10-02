cc13641359e77a12c44272a8fe49d8de348cd031
imran-laborjack <isoteriksoftware@gmail.com>
2023-10-02 12:30:06 +0100

    update

---
+++ src/main/java/com/isoterik/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,29 @@
+package com.isoterik.microservices.currencyexchangeservice;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+    private final Environment environment;
+    private final CurrencyExchangeRepository repository;
+
+    public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository repository) {
+        this.environment = environment;
+        this.repository = repository;
+    }
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+        if (currencyExchange == null)
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        
+        currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+
+        return currencyExchange;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values(10001,'USD','NGN',950,'');
+
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values(10002,'USD','INR',65,'');
+
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values(10003,'USD','EUR',1.5,'');
